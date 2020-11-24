4b46b99a5261d65d4178ecc4bd41d4fd76f56668
denis Nasution <dnisnast@gmail.com>
2020-11-24 21:39:49 +0700

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -30,10 +30,11 @@ app.get('/api/config/paypal', (req, res) => {
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname,'/uploads')))
-
-app.get('/', (req, res) => {
-    res.send("Bacend Server")
-})
+app.use(express.static(path.join(__dirname, '/frontend/build')))
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/frontend/build/index.html')))
+// app.get('/', (req, res) => {
+//     res.send("Bacend Server")
+// })
 
 app.use((err, req, res, next) => {
     res.status(500).send({message: err.message})
